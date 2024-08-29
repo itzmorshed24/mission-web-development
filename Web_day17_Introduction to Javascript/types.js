ca3d664b904eca20eebe5e29c70471eea40f6dd9
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-29 10:01:14 +0600

    learned Numeric, String, Boolean

---
+++ Web_day17_Introduction to Javascript/types.js
@@ -0,0 +1,17 @@
+// String Types
+var name = "Morshed Alam";
+// console.log(name);
+var address = "Adar Killa Bandarban with some katabon";
+var country = "Bangladesh";
+
+// Boolean
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+var price = 555;
+console.log(typeof price);
