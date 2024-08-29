71f77f0cc1cfe10322e10f7fa1b9babe95a7ef9f
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-29 09:41:21 +0600

    leared variable decleare

---
+++ Web_day17_Introduction to Javascript/variable.js
@@ -0,0 +1,21 @@
+/**
+ * VARIABLE ==> vary + able
+ * 5 things to declare a variable
+ */
+
+var weight = 34;
+var price = 38;
+var year = 1995;
+var money = 2005;
+money = 500;
+age =45;
+var age;
+console.log(age);
+console.log(money);
+console.log(price);
+
+
+
+var age = 24;
+age = 35;
+console.log(age);
