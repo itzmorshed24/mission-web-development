6b1475fd267cd1596bc6b019714657a20f35d552
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-30 09:47:25 +0600

    learned numbers & keyowrd: typeof, parseInt, parseFloat, toFixed()

--- Web_day17_Introduction to Javascript/number.js
+++ Web_day17_Introduction to Javascript/number.js
@@ -1,2 +1,19 @@
-asdf
-asdf
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = parseFloat('24.5');
+var eggPrice = parseInt('50');
+
+// console.log(typeof orangePrice);
+// console.log(applePrice);
+// console.log(eggPrice);
+// console.log(orangePrice+applePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+// console.log(typeof total.toFixed(1));
+// console.log(parseFloat(total.toFixed(1)));
+
+const num = -50/0;
+console.log(num);
