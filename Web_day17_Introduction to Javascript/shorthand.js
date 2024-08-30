cabad1333cb62f31978684c19100dfe8b14a682e
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-30 20:51:20 +0600

    learned js basic shorthand

---
+++ Web_day17_Introduction to Javascript/shorthand.js
@@ -0,0 +1,30 @@
+var price = 35;
+// price = price+10;
+price +=20;
+// console.log(price);
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 2000;
+// console.log(fridge);
+
+// multiply
+var orangePrice = 30;
+// orangePrice = orangePrice * 3;
+orangePrice *=3;
+// console.log(orangePrice);
+
+// Division
+var first = 'Mobarok';
+var second = 'Tobarok';
+
+var fullName = first * second;
+
+// console.log(fullName);
+
+// console.log(40+'2');
+// console.log(40-'20');
+// 
+
+console.log(fullName);
+
