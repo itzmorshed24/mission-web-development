e368743a1184bac4587403af678eb3cad8ac7f1e
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-30 09:31:39 +0600

    learned variable naming rules

---
+++ Web_day17_Introduction to Javascript/naming.js
@@ -0,0 +1,30 @@
+// No Keyword in Variable Name
+var false = 45;
+var var = 50;
+
+// No space or gap in Variable
+var is happy = false;
+var isHappy = true;
+
+//  No Quote
+var "address" = "Khochu khet"
+
+// Can not start with a number. but number other than the first letter is allowed
+var money = 45; !true
+var money2 = 55; !true
+var 3money = 88; !false
+
+// Name is case sensitive
+address
+Address
+ADDRESS
+addRess
+ADDress
+
+// how write a long variable name
+var my current home address = "Andorkilla Bandar Ban"
+var my_current_home_address ="Andorkilla"  //sanke case
+var MyCurrentHomeAddress = "Andorkilla" // pascle case
+var myCurrentHomeAddress ="Andorkilla" //camel case
+
+
