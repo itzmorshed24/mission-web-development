27119500e7c00718f2f6055adc815ba4f379e841
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-31 09:25:59 +0600

    learned ternary operator

---
+++ Web_day18_conditional/ternary.js
@@ -0,0 +1,26 @@
+// if(age >=18){
+//     console.log('You can vote.');
+// }
+// else{
+//     console.log('Ghumai thako');
+// }
+
+/**
+ * condition ? do something when true : do something when false;
+ */
+// simple ternary
+// const age = 12;
+// age>=18 ? console.log('vote dio') : console.log('Ghumai THako');
+
+let price = 500;
+const isLeader = false;
+
+// if(isLeader ===true){
+//     price = 0;
+// }
+// else{
+//     price =price +100;
+// }
+// console.log(price);
+
+price = isLeader ===true ? 0 : price +100;
