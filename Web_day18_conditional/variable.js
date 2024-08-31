422324b0988f4e0d059fa53495754048ffc6049d
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-31 07:22:46 +0600

    revision previous day & learned comparision oparetor

---
+++ Web_day18_conditional/variable.js
@@ -0,0 +1,33 @@
+/**
+ * VARIABLE ==> Container
+ */
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = "CSE";
+
+// boolean
+var passed = true;
+
+/**
+ *  Naming Conversion
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+
+var lastExamResult = 60;
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2.+=, -=, *=, /=
+ * 3. isNan?
+ * 4. null
+ * 5. Number
+ */
