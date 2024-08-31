5bd6d922e2c07d60db1057c5df3c394c74957da5
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-31 08:17:44 +0600

    learned if & else condition

---
+++ Web_day18_conditional/if.js
@@ -0,0 +1,48 @@
+/**
+ * Tui jodi amake khawas
+ * tahole ami asbo
+ * 
+ * -------------------
+ * if you buy food for me 
+ *      i will come
+ * 
+ * 
+ * -----------------
+ * 
+ * if briyani price less than 300 taka
+ *          i will buy
+ * 
+ * if (condition){
+ *  condition jodi ture hoy tahole print hobe
+ * }
+ */
+
+
+// if(3<10){
+//     // console.log('i a smaller value.');
+// }
+
+// var briyaniPrice = 250;
+
+// if(briyaniPrice < 300){
+//     console.log('mama give me some briyani');
+// }
+
+
+
+
+
+var weight = 20;
+
+if(weight <= 20){
+    console.log('I will carry it by myself');
+}
+else{
+    console.log('I will rent a rickshaw');
+}
+
+
+
+
+
+ 
