27d19ee1ce4fae7a8d62d4d71ab0452537150a6a
itzmorshed24 <itzmorshed24@gmail.com>
2024-08-31 07:38:15 +0600

    learned comparision

---
+++ Web_day18_conditional/comparision.js
@@ -0,0 +1,24 @@
+// console.log(5 >10);
+// console.log(5 <10);
+// console.log(5==10);
+// console.log(10==10);
+// console.log(8>=10);
+// console.log(5<=10);
+// console.log(15<=10);
+// condole.log(10==10.1);
+
+
+var peyaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+// console.log(applePrice <peyaraPrice);
+
+/**
+ * ==
+ * ===
+ */
+
+
+console.log(10=='10');
+console.log(10==='10')
