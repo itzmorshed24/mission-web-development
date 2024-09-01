0f0518d01534ff2c214a0f45e6a913ea45da2fa8
itzmorshed24 <itzmorshed24@gmail.com>
2024-09-01 09:21:09 +0600

    learned array delecare

---
+++ Web_day18.5_array/array.js
@@ -0,0 +1,9 @@
+
+/**
+ * Array 
+ * []----------third bracket use kora hoy
+ */
+const numbers = [12, 22, 44, 55, 66, 77];
+const friends = ['abul', 'babul', 'morshed', 'rashed'];
+const passed = [true, false, true, false];
+const mixedArray = ['abul', 22, 'kabul', true, 3.25];
