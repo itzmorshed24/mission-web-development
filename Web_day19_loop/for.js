de9f0e7c3bc580ee0266bb2633732443ba89eb26
itzmorshed24 <itzmorshed24@gmail.com>
2024-09-02 07:38:45 +0600

    learned for & while loop

---
+++ Web_day19_loop/for.js
@@ -0,0 +1,20 @@
+// const numbers = [12, 98, 45, 65, 75, 88, 45, 20, 36, 63, 21,72];
+
+// console.log(numbers);
+
+// for(const result of numbers){
+//     console.log(result);
+// }
+
+/**
+ * for(){
+ * 
+ * }
+ */
+
+const fruits =['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat.');
+}
---
+++ Web_day19_loop/while.js
@@ -0,0 +1,15 @@
+const number = 7;
+if(number <1){
+    console.log('choto ekta number paisi');
+}
+console.log('something new is cooking');
+
+
+// ***********WHILE------ LOOP------------------
+let num = 0;
+while (num <=5){
+    console.log('Looping', num);
+    // num = num + 1;
+    // num+=1;
+    num++;
+}
