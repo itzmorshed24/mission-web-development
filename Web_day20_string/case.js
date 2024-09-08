323bdb5479e0f6954d96ebbbee6677604bb681df
itzmorshed24 <itzmorshed24@gmail.com>
2024-09-08 09:58:15 +0600

    learned js case

---
+++ Web_day20_string/case.js
@@ -0,0 +1,31 @@
+const school ='Dinajpur Polytechnic Institute';
+
+console.log(school.toLowerCase());
+console.log(school.toLowerCase());
+
+
+// uppercase : ABCD EFG
+// lowercase : acd efg
+
+
+const subject ='Chemistry';
+const book = 'chemistry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('I am reading book');
+}
+else{
+    console.log('hudai hudai prista ultai ar vat pawa jabe na');
+}
+
+
+
+const drink = ' water';
+const liquid = 'water ';
+
+if(drink.trim() === liquid.trim()){
+    console.log('Pani er opor name life');
+}
+else{
+    console.log('somudre pani kaite pari na');
+}
