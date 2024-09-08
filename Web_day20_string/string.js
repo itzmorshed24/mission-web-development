81198df8b78867acd3398106e2c54bf459fd3e3d
itzmorshed24 <itzmorshed24@gmail.com>
2024-09-08 09:08:15 +0600

    learned string

---
+++ Web_day20_string/string.js
@@ -0,0 +1,21 @@
+const country = 'Bangladesh';
+const division = "Rangpur";
+const upazilla = `Chirirbandar`;
+// const union = new String('Amarpur');
+
+// console.log(typeof union);
+
+const number =[48,58,78,21,65];
+
+console.log(number);
+// console.log(number.length);
+// console.log(number[3]);
+number[1] = 505;
+console.log(number);
+
+const capital = 'Dhaka';
+console.log(capital);
+// console.log(capital.length);
+// console.log(capital[3]);
+capital[0] = 'F';
+console.log(capital);
